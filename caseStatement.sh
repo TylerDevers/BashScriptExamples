5a961f2a70f1e51cec949e4ed06f63c7df9d02f7
Tyler Devers <devers.tyler@gmail.com>
2019-06-12 14:04:56 -0700

    add caseStatement.sh

---
+++ caseStatement.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Example of a case statement
+
+#Ensure script was passed an argument
+[[ $# -ne 1 ]] && \
+     echo "Error: You must pass a number 1 - 12" && exit 1
+
+#Begin script
+month=$1
+
+case $month in
+    1)		echo January ;;
+    2)		echo February ;;
+    3)		echo March ;;
+    4)		echo April ;;
+    5)		echo May ;;
+    6)		echo June ;;
+    7)		echo July ;;
+    8)		echo August ;;
+    9)		echo September ;;
+    10)		echo October ;;
+    11)		echo Novemeber ;;
+    12)		echo Decemember ;;
+    *)		echo "No month matches $month" && exit 2 ;; 
+esac
+
--- stringTest.sh
+++ stringTest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Check that two arguments were passed
+#Compact form of an if statement
+[[ $# -ne 2 ]] && \
+     echo "give two strings as an argument" && exit 1
+
 # Functions
 checkLength () {
      if [ ${#1} -eq 0 ] ; then # check length of passed variable
@@ -10,12 +15,16 @@ checkLength () {
 }
 
 # Get two arguments from the user
-echo Please enter the first argument: 
-read arg1
-echo Please enter the second argument:
-read arg2
+arg1=$1
+arg2=$2
 
 # Check arguments for length
 echo First argument: ; checkLength $arg1
 echo Second Argument: ; checkLength $arg2
 
+# Check if arguments are equal
+if [ $arg1 == $arg2 ] ; then 
+     echo the two arguments are the same
+else
+     echo the two arguments are different
+fi
