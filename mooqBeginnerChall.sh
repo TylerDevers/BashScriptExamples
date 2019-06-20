dfd964913ce5b39e97b2a6c94f1784cbbb718c58
Tyler Devers <devers.tyler@gmail.com>
2019-06-20 09:48:36 -0700

    mookBeginnerChall.sh - TODO sort function not working

--- mooqBeginnerChall.sh
+++ mooqBeginnerChall.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-# Declare functions
+## Declare functions
 
 randomData() {
+     # get random characters from urandom
+     # pipe this data through the translate command to keep only latin chars
+     # pipe through head to limit characters to a count of 15
      echo $(cat /dev/urandom | tr -dc '0-9a-zA-Z' | head -c 15)
 }
 
-# Make output file and define file size for tracking purposes
-# Test if output file exists, if it does, empty it
+sortData() {
+     unsortedFile= $1
+     $( sort -d unsortedFile -o sortedMooq.out )
+#     $( head sortedMooq.out )
+#     $( tail sortedMooq.out ) 
+}
+
+## Make output file and define file size for tracking purposes
+# Test if output file exists, if it does, empty the file. If it does not, create the file
 if [ -f mooq.out ] ; then 
-    $( >mooq.out )
+     # empty mooq.out
+     $( >mooq.out )
 else
+     # create mooq.out
      touch mooq.out
 fi
 
-# Add random latin characters to output file, lines are 15 character maximum 
-file=mooq.out
-fileSize=$( wc -c <$file )
-while [ $fileSize -le 1000000 ]
+## Add random latin characters to output file, lines are 15 character maximum 
+# create a variable to access the output file
+outputFile=mooq.out
+# save the size of the output file in bytes
+fileSize=$( wc -c <$outputFile)
+# Stop appending to file when file reaches 1MB in size using a while loop
+# I chose the while loop because it is human readable
+while [ $fileSize -le 1000 ]
 do 
+     # append new line of random data to output file, then re-evaluate fileSize
      randomData >> mooq.out
-     fileSize=$( wc -c <$file ) 
-     i=$(( $i+1 ))
+     fileSize=$( wc -c <$outputFile) 
 done
-echo "file size has exceeded 1M: aborting.... "
-echo "mooq size is:  $(wc -c < $file )"
+
+## File size limit has been reached, notify user
+echo "file size has exceeded 1M: Beginning sort... "
+echo "mooq size is:  $(wc -c < $outputFile)"
+
+# sort the output file in dictionary order, since it is a common sort order
+sortData $outputFile
+
 exit 0
 
